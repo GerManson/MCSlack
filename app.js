7f8aad025186c5a8f9f193cbc3f08588186b702d
German Rodriguez <german@phisoft.com.mx>
2016-03-01 19:42:52 -0700

    Slack connection

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+// Dependencies
 var express = require('express');
-var app = express();
+var dotenv = require('dotenv');
+var Slack = require('slack-node');
 
+dotenv.load();
+
+// Configurations
+apiToken = process.env.SLACK_TOKEN;
+
+// Initializations
+var app = express();
+slack = new Slack(apiToken);
+
+// routes and responses
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  slack.api('chat.postMessage', {
+    text:'hello from nodejs',
+    channel:'#random'
+  }, function(err, response){
+    console.log(response);
+  });
+
+  res.send('Server Running...!');
 });
 
+
+// Server Start
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('MCSlack Server listening on port 3000!');
 });
