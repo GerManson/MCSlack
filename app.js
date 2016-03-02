c1184a63f41dc64801435c4ded8c2114a5a4474e
German Rodriguez <german@phisoft.com.mx>
2016-03-01 20:13:58 -0700

    Slack Real Time Messaging API Integration

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 // Dependencies
 var express = require('express');
 var dotenv = require('dotenv');
-var Slack = require('slack-node');
 
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
+
+
+// Loading environment configurations (.env file)
 dotenv.load();
-
-// Configurations
-apiToken = process.env.SLACK_TOKEN;
+var token = process.env.SLACK_TOKEN || '';
 
 // Initializations
 var app = express();
-slack = new Slack(apiToken);
+var rtm = new RtmClient(token, {logLevel: 'live'});
+rtm.start();
+
 
 // routes and responses
 app.get('/', function (req, res) {
-
-  slack.api('chat.postMessage', {
-    text:'hello from nodejs',
-    channel:'#random'
-  }, function(err, response){
-    console.log(response);
-  });
-
   res.send('Server Running...!');
 });
 
+
+// LISTENING FOR SLACK EVENTS
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+
+  // reply
+  rtm.sendMessage('soy tu espejo: ' + message.text, message.channel, function messageSent() {
+    console.log(message);
+  });
+
+});
+
+rtm.on(RTM_EVENTS.CHANNEL_CREATED, function (message) {
+  // Listens to all `channel_created` events from the team
+});
 
 // Server Start
 app.listen(3000, function () {
